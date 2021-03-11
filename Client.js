65e40be1768b381be4f1b0e5b84c140c2aeba7cd
NULO-ux <68197361+NULO-ux@users.noreply.github.com>
2021-03-11 12:26:46 -0300

    Status

--- Client.js
+++ Client.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js');
 const token = require('./token.json'); 
+const pre = require("./json/prefix.json");
 const client = new Discord.Client();
 const ColorMusic = require('./Music/src/SondMix.js');
 const bot = new ColorMusic({
-    botPrefix:"c!",
+    botPrefix:".p",
     ytApiKey:"AIzaSyAIBEppMcUFtTpXI8M9jWD0fK71Vr0pw5M",
     botClient: client
 })
@@ -13,7 +14,26 @@ client.on('message', message =>{
     }
 });
 
-client.on('ready', () => {
-    console.log(`Logado em ${client.guilds.cache.size} servidores!`)
+client.on("ready", () => {
+    console.log(`Tocando em ${client.guilds.cache.size} Servidores, ${client.channels.cache.size} canais.`)
+    let activities = [
+        `Use .p`,
+        `Tocando em ${client.guilds.cache.size} servidores.`,
+        `${client.channels.cache.size} canais.`,
+    ],
+        i = 0;
+    setInterval(() => client.user.setActivity(`${activities[i++ %
+        activities.length]}`, {
+        type: "PLAYING"
+    }), 5000);//WATCHING, LISTENING, PLAYING, STREAMING
+    client.user.setStatus("idle")
+});
+client.on("guildCreate", guild => {
+    console.log(`O bot entro nos servidor: ${guild.name} (id: ${guild.id}). População: ${guild.memberCount} menbros`);
+    client.user.setActivity(`Estou em ${client.guilds.size} servidores`);
+});
+client.on("guildDelete", guild => {
+    console.log(`O bot foi removido do servidor: ${guild.name} (id: ${guild.id})`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 client.login(token.token)
